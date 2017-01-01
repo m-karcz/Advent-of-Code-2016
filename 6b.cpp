30413c283170f1548bee415a429748d6c946920c
Mariusz Karcz <ujemnypozyton@gmail.com>
2017-01-01 12:40:58 +0100

    Day 6

---
+++ 6b.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <algorithm>
+#include <climits>
+
+int main(){
+	std::map<std::size_t, std::map<char, int>> counter;
+	std::fstream input;
+	input.open("6a.txt");
+	std::string line;
+	while(std::getline(input, line)){
+		for(std::size_t i=0; i<line.size(); i++){
+			counter[i][line[i]]++;
+		}
+	}
+	input.close();
+	for(auto& column : counter){
+		std::pair<char, int> most_common=std::make_pair<char, int>('\0', INT_MAX);
+		for(auto& pair : column.second){
+			if(most_common.second > pair.second)
+				most_common=pair;
+		}
+		std::cout << most_common.first;
+	}	
+	std::cout << std::endl;
+	return 0;
+}
